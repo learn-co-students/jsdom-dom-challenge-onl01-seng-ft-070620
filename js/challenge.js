cbe0b53fc881af9e1bd370b98d55ea6f4cfed59c
Amanda-Katherine <walker.akw@gmail.com>
2020-10-14 02:08:31 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,26 +1,53 @@
-"use strict";function _toConsumableArray(a){
-    if(Array.isArray(a)){for(var b=0,c=Array(a.length);b<a.length;b++)c[b]=a[b];return c}return Array.from(a)}var playing=!0,
-    
-    timer=function(){return setInterval(function(){var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b+1},1e3)},
-    
-    interval=timer(),
-    
-    minus=document.getElementById("minus"),
-    
-    plus=document.getElementById("plus"),
-    
-    heart=document.getElementById("heart"),
-    
-    pause=document.getElementById("pause"),
-    
-    commentForm=document.getElementsByTagName("form")[0];
-    
-    minus.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b-1}),
-    
-    plus.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText);a.innerText=b+1}),
-    
-    heart.addEventListener("click",function(){var a=document.getElementById("counter"),b=parseInt(a.innerText),c=document.querySelector(".likes"),d=void 0;if([].concat(_toConsumableArray(c.children)).map(function(a){return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');var e=parseInt(d.children[0].innerText);d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)}),
-    
-    pause.addEventListener("click",function(){playing?(playing=!1,clearInterval(interval),this.innerText="resume"):(playing=!0,interval=timer(),this.innerText="pause"),[].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){"pause"!==a.id&&(a.disabled=!playing)})}),
-    
-    commentForm.addEventListener("submit",function(a){a.preventDefault();var b=this.children[0],c=b.value;b.value="";var d=document.querySelector(".comments"),e=document.createElement("p");e.innerText=c,d.appendChild(e)});
+const counter = document.getElementById('counter');
+const minus = document.getElementById('minus');
+const plus = document.getElementById('plus');
+const heart = document.getElementById('heart');
+const pause = document.getElementById('pause');
+
+const displayCounter = function() {
+  const counterNum = parseInt(counter.innerText);
+  counter.innerText = counterNum + 1;
+  minus.addEventListener('click', minusCounter);
+  plus.addEventListener('click', plusCounter);
+  pause.addEventListener('click', pauseCounter);
+  heart.addEventListener('click', like);
+}
+
+const minusCounter = function() {
+  const counterNum = parseInt(counter.innerText);
+  counter.innerText = counterNum - 1;
+}
+
+const plusCounter = function() {
+  const counterNum = parseInt(counter.innerText);
+  counter.innerText = counterNum + 1;
+}
+
+const like = function() {
+  let likesUl = document.getElementsByClassName('likes')[0];
+  const newItem = document.createElement('li');
+  let displayLike = `${counter.innerText} has been liked`;
+  newItem.innerText = displayLike;
+  likesUl.appendChild(newItem);
+}
+
+function pauseCounter() {
+  clearInterval(timer);
+  minus.disabled = true;
+  plus.disabled = true;
+  heart.disabled = true;
+  pause.innerText = "resume";
+  timerStatus = 0;
+  pause.addEventListener('click', resumeTimer);
+}
+
+function resumeTimer() {
+  setInterval(displayCounter, 1000);
+  minus.disabled = false;
+  plus.disabled = false;
+  heart.disabled = false;
+  pause.innerText = "pause";
+  pause.addEventListener('click', pauseCounter);
+}
+
+let timer = setInterval(displayCounter, 1000);
